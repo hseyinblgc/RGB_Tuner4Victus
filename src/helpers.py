520c1e3e949f24e7ee48f454557b740ba8281704
hseyinblgc <h.bilgic1818@gmail.com>
2026-07-25 16:56:03 +0300

    refactor(color): move parsing logic into helpers module
    - Extract `PRESET_COLORS` and `parse_color` from `victus-rgb.py`
    - Relocate color handling logic to `src/helpers.py` for better organization

--- src/helpers.py
+++ src/helpers.py
@@ -3,6 +3,7 @@
 # --------------------------
 import colorsys
 import subprocess
+import sys
 
 
 def speed_delay(speed: int) -> float:
@@ -32,3 +33,48 @@ def kill_previous():
         stderr=subprocess.DEVNULL,
         check=False,
     )
+
+
+
+PRESET_COLORS = {
+    "red": (255, 0, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "cyan": (0, 255, 255),
+    "purple": (255, 0, 255),
+    "neon-purple": (100, 12, 223),
+    "white": (255, 255, 255),
+    "fire": (255, 60, 0),
+    "flame": (255, 90, 0),
+    "off": (0, 0, 0),
+}
+
+
+def parse_color(values: list[str]) -> list[tuple]:
+    match values:
+        case [color] if color.lower() in PRESET_COLORS:
+            return [PRESET_COLORS[color.lower()]]
+
+        case [color1, color2] if (
+            color1.lower() in PRESET_COLORS and color2.lower() in PRESET_COLORS
+        ):
+            return [PRESET_COLORS[color1.lower()], PRESET_COLORS[color2.lower()]]
+
+        case [r, g, b]:
+            try:
+                return [(int(r), int(g), int(b))]
+            except ValueError:
+                print("RGB values must be integers")
+                sys.exit(1)
+
+        case [r1, g1, b1, r2, g2, b2]:
+            try:
+                return [(int(r1), int(g1), int(b1)), (int(r2), int(g2), int(b2))]
+            except ValueError:
+                print("RGB values must be integers")
+                sys.exit(1)
+
+        case _:
+            usage()
+
--- victus-rgb.py
+++ victus-rgb.py
@@ -11,7 +11,7 @@ from src.ea_access import (
     write_rgb,
 )
 from src.effects import alternate, breathe, fade, rainbow
-from src.helpers import kill_previous
+from src.helpers import kill_previous, parse_color
 
 parser = argparse.ArgumentParser(
     prog="victus-rgb",
@@ -42,50 +42,6 @@ p_fade = sub.add_parser("fade", help="Fade between two colors.")
 p_fade.add_argument("color", nargs="+")
 
 args = parser.parse_args()
-
-
-PRESET_COLORS = {
-    "red": (255, 0, 0),
-    "green": (0, 255, 0),
-    "blue": (0, 0, 255),
-    "yellow": (255, 255, 0),
-    "cyan": (0, 255, 255),
-    "purple": (255, 0, 255),
-    "neon-purple": (100, 12, 223),
-    "white": (255, 255, 255),
-    "fire": (255, 60, 0),
-    "flame": (255, 90, 0),
-    "off": (0, 0, 0),
-}
-
-
-def parse_color(values: list[str]) -> list[tuple]:
-    match values:
-        case [color] if color.lower() in PRESET_COLORS:
-            return [PRESET_COLORS[color.lower()]]
-
-        case [color1, color2] if (
-            color1.lower() in PRESET_COLORS and color2.lower() in PRESET_COLORS
-        ):
-            return [PRESET_COLORS[color1.lower()], PRESET_COLORS[color2.lower()]]
-
-        case [r, g, b]:
-            try:
-                return [(int(r), int(g), int(b))]
-            except ValueError:
-                print("RGB values must be integers")
-                sys.exit(1)
-
-        case [r1, g1, b1, r2, g2, b2]:
-            try:
-                return [(int(r1), int(g1), int(b1)), (int(r2), int(g2), int(b2))]
-            except ValueError:
-                print("RGB values must be integers")
-                sys.exit(1)
-
-        case _:
-            usage()
-
 
 # --------------------------
 # CLI
